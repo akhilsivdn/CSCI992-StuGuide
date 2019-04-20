b355bf1b8b7e00d0faa17f6a08d1444afa284c39
Ankit <ankitpanagar01@gmail.com>
2019-04-20 16:22:31 +1000

    UI update on Latest Code
    NavBar Icons Login Register UI update

--- components/login.js
+++ components/login.js
@@ -1,31 +1,32 @@
 import React from "react";
 import {Link}  from "react-router-dom";
-
+import { TextField, Button } from '@material-ui/core';
 
 export class LoginComponent extends React.Component{
-   
+
     constructor() {
         super();
       }
 
-  
+
     myFunction(){
-        
+
         }
-   
-   
+
     render(){
         return(
             <div className="loginSection">
-               <input placeholder="student id" className="inputfield" type="text" />
-               <input placeholder="password" className="inputfield pos" type="password"/>
+
+               <TextField label="Username" margin="dense" placeholder="Enter Username" className="inputfield" type="text" />
+
+               <TextField label="Password" margin="dense" placeholder="Enter Password" className="inputfield pos" type="password"/>
                <Link to={'/home'}>
-                     <button className="pos" onClick={this.myFunction}>Login</button>
+                     <Button size="small" color="primary" variant="contained" className="pos" onClick={this.myFunction}>Login</Button>
                </Link>
                <Link to={'/register'}>
-                     <button className="pos" onClick={this.myFunction}>Register</button>
+                     <Button size="small" color="secondary" variant="contained" className="pos" onClick={this.myFunction}>Register</Button>
                </Link>
-            </div>   
+            </div>
         );
     }
 }
