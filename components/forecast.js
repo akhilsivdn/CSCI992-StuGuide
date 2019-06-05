40f8d70e280ff23fc92ddbe7e50ca4966fbd0b1d
Akhil <akhil2369492@gmail.com>
2019-06-05 14:12:28 +1000

    UI update - forecast page

--- components/forecast.js
+++ components/forecast.js
@@ -23,37 +23,32 @@ export class ForecastComponent extends React.Component {
         let dayString = '';
         switch (day) {
             case 0:
-                dayString = "Sun";
+                dayString = "Sunday";
                 break;
             case 1:
-                dayString = "Mon";
+                dayString = "Monday";
                 break;
             case 2:
-                dayString = "Tue";
+                dayString = "Tuesday";
                 break;
             case 3:
-                dayString = "Wed";
+                dayString = "Wednesday";
                 break;
             case 4:
-                dayString = "Thu";
+                dayString = "Thursday";
                 break;
             case 5:
-                dayString = "Fri";
+                dayString = "Friday";
                 break;
             case 6:
-                dayString = "Sat";
+                dayString = "Saturday";
                 break;
         }
         return dayString;
     }
 
     GetIt() {
-        try {
-            var location = localStorage.getItem('locationName');
-        }
-        catch (error) {
-            console.log(error);
-        }
+        var location = localStorage.getItem('locationName');
 
         let newDate = new Date()
         this.setState({
@@ -82,54 +77,66 @@ export class ForecastComponent extends React.Component {
             console.log(error);
         }
 
-        if(this.state.isLoading){
-            return(
+        if (this.state.isLoading) {
+            return (
                 <div className="loadingBar">
-                <Modal
-                    open={this.state.isLoading}
-                    style={{
-                        transitionDuration: '800ms',
-                        transitionDelay: '800ms'
-                    }}>
-                    <CircularProgress
+                    <Modal
+                        open={this.state.isLoading}
                         style={{
-                            position: 'absolute',
-                            top: '45%',
-                            left: '47%',
-                            color: '#1f41fa',
-                        }}
-                        thickness={4}
-                        size={70}
-                    />
-                </Modal>
-            </div>
+                            transitionDuration: '800ms',
+                            transitionDelay: '800ms'
+                        }}>
+                        <CircularProgress
+                            style={{
+                                position: 'absolute',
+                                top: '45%',
+                                left: '47%',
+                                color: '#1f41fa',
+                            }}
+                            thickness={4}
+                            size={70}
+                        />
+                    </Modal>
+                </div>
             )
         }
 
         return (
             <div className="forecastpage">
                 <div>
-                    <div>
+                    <div align="center">
+                        <br />
+                        <img src={'https://cdn3.iconfinder.com/data/icons/unicons-vector-icons-pack/32/location-512.png'} width={'18px'} height={'18px'}></img>
                         <br />
                         <font size={'5'}><b>{location}</b></font>
                     </div>
                     <hr />
 
-                    <div className="forecast_current">
-                        <div className="current_weather_text">
-                            <span style={{
-                                display: 'block'
-                            }}>        <font size={'5'}>{this.state.current && this.state.current.condition.text}</font></span>
-                            <span>        <font size={'6'}><b>{this.state.current && this.state.current.temp_c}℃</b></font></span>
-                        </div>
-                        <div className="current_weather_image">
-
-                            <img src={this.state.current && this.state.current.condition.icon} width={'120px'} height={'120px'}></img>
-                        </div>
+                    <div align="center">
+                        <span><font size={'10'}><b>{this.state.current && this.state.current.temp_c}&#176;</b></font></span>
+                        &nbsp;&nbsp;&nbsp;&nbsp;
+                        <span>
+                            <img src={this.state.current && this.state.current.condition.icon} style={{
+                                width: "75px",
+                                height: "75px",
+                                verticalAlign: "top"
+                            }}></img>
+                        </span>
                     </div>
 
-                    <div className="forecastDays">
+                    <div align="center" style={{ color: "gray" }}>
+                        <font size={'3'}>
+                            <img src={'https://image.flaticon.com/icons/svg/727/727790.svg'} width={'18px'} height={'18px'}></img>Humidity:&nbsp;{this.state.current && this.state.current.humidity}%&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            <img src={'https://image.flaticon.com/icons/svg/56/56086.svg'} width={'18px'} height={'18px'}></img> Wind:&nbsp;{this.state.current && this.state.current.wind_kph}km/h
+                        </font>
+                    </div>
 
+                    <div align="center">
+                        <span style={{ display: 'block' }}><font size={'4'}>{this.state.current && this.state.current.condition.text}</font></span>
+                    </div>
+                    <hr />
+
+                    <div align="center">
                         {
                             this.state.forecastDays.map(function (forecastDay, i) {
                                 if (i == 0) {
@@ -143,22 +150,28 @@ export class ForecastComponent extends React.Component {
                                 this.state.day++;
 
                                 return (
-                                    <div className="forecast">
-                                        <div><font size={'5'}>{dayString}</font></div>
-                                        <div className="current_weather_image">
-                                            <img src={forecastDay.day.condition.icon} width={'100px'} height={'100px'}></img>
+                                    <div>
+                                        <div style={{ float: 'left' }}>
+                                            <div><font size={'5'}>&nbsp;&nbsp;&nbsp;&nbsp;{dayString}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</font></div>
+                                            <div align="left" style={{
+                                                color: "gray"
+                                            }}><font size={'3'}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{forecastDay.day.condition.text}</font></div>
+                                            <div><font size={'3'}>&nbsp;</font></div>
                                         </div>
-                                        <div>
-                                            <div className="current_weather_text">
-                                                <div className="conditionText"><b>{forecastDay.day.maxtemp_c}℃</b></div>
-                                                <div className="conditionText">{forecastDay.day.mintemp_c}℃</div>
+                                        <div style={{ float: 'right' }}>
+                                            <div style={{ float: 'left' }}>
+                                                <img src={forecastDay.day.condition.icon} width={'50px'} height={'50px'}></img>
+                                            </div>
+                                            <div style={{ float: 'right' }}>
+                                                <div className="conditionText"><font size={'4'}><b>{forecastDay.day.maxtemp_c}&#176;&nbsp;&nbsp;&nbsp;&nbsp;</b></font></div>
+                                                <div className="conditionText"><font size={'3'}>{forecastDay.day.mintemp_c}&#176;&nbsp;&nbsp;&nbsp;&nbsp;</font></div>
                                             </div>
                                         </div>
                                     </div>
                                 )
                             }, this)
                         }
-
+                        &nbsp;&nbsp;&nbsp;&nbsp;
                     </div>
                 </div>
             </div>
