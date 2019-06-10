511bcf284932645125bfaa45c05e6a4f128c9adb
Ankit <ankitpanagar01@gmail.com>
2019-06-10 12:23:45 +1000

    Settings Page

--- components/settings.js
+++ components/settings.js
@@ -1,6 +1,17 @@
 import React from "react";
 import axios from 'axios';
+import AvatarMUI from '@material-ui/core/Avatar';
+import Chip from '@material-ui/core/Chip';
+import FaceIcon from '@material-ui/icons/Face';
 import Avatar from 'react-avatar-edit';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelActions from "@material-ui/core/ExpansionPanelActions";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { Divider, Typography, Button } from "@material-ui/core";
 
 export class SettingsComponent extends React.Component {
     constructor(props) {
@@ -58,9 +69,123 @@ export class SettingsComponent extends React.Component {
             : '';
         return (
             <div>
-                <h1>Profile Settings</h1>
+                <div className="title_page">Account Settings</div>
                 {error}
-                <form novalidate>
+
+                <div>
+                    <ExpansionPanel
+                        style={{ marginBottom: '10px' }}>
+                        <ExpansionPanelSummary
+                            style={{
+                                fontSize: 'large',
+                                fontWeight: '400'
+                            }}
+                            expandIcon={<ExpandMoreIcon />}>
+                            Update Profile Picture
+                        </ExpansionPanelSummary>
+
+                        <ExpansionPanelDetails>
+                            Current:
+                        </ExpansionPanelDetails>
+
+                        <ExpansionPanelDetails>
+                            Preview:
+                        </ExpansionPanelDetails>
+
+                        <Divider />
+
+                        <ExpansionPanelActions>
+                            <Button size="small">Cancel</Button>
+                            <Button size="small" color="primary">
+                                Save
+                            </Button>
+                        </ExpansionPanelActions>
+                    </ExpansionPanel>
+                </div>
+
+                <div>
+                    <ExpansionPanel
+                        style={{ marginBottom: '10px' }}>
+                        <ExpansionPanelSummary
+                            style={{
+                                fontSize: 'large',
+                                fontWeight: '400'
+                            }}
+                            expandIcon={<ExpandMoreIcon />}>
+                            Update Username
+                        </ExpansionPanelSummary>
+
+                        <ExpansionPanelDetails>
+                            <div style={{marginLeft:"10px"}}>Current: </div>{/* <Chip label="Username" /> */}
+                            <TextField
+                                style={{marginTop:"10px"}}
+                                label="Enter New Username"
+                                placeholder="Enter New Username"
+                                type="text"
+                                name="Username"
+                                margin="Dense"
+                            />
+                        </ExpansionPanelDetails>
+
+                        <ExpansionPanelDetails>
+                            {/* <TextField
+                                label="Enter New Username"
+                                placeholder="Enter New Username"
+                                type="text"
+                                name="Username"
+                                margin="Dense"
+                            /> */}
+                        </ExpansionPanelDetails>
+                        <Divider />
+                        <ExpansionPanelActions>
+                            <Button size="small">Cancel</Button>
+                            <Button size="small" color="primary">
+                                Save
+                            </Button>
+                        </ExpansionPanelActions>
+                    </ExpansionPanel>
+                </div>
+
+                <div>
+                    <ExpansionPanel
+                        style={{ marginBottom: '10px' }}>
+                        <ExpansionPanelSummary style={{
+                            fontSize: 'large',
+                            fontWeight: '400'
+                        }}
+                            expandIcon={<ExpandMoreIcon />}>
+                            Update Password
+                        </ExpansionPanelSummary>
+
+                        <ExpansionPanelDetails>
+                            Current:
+                        </ExpansionPanelDetails>
+
+                        <ExpansionPanelDetails>
+                            New:
+                        </ExpansionPanelDetails>
+
+                        <ExpansionPanelDetails>
+                            Confirm:
+                        </ExpansionPanelDetails>
+
+                        <Divider />
+
+                        <ExpansionPanelActions>
+                            <Button size="small">Cancel</Button>
+                            <Button size="small" color="primary">
+                                Save
+                            </Button>
+                        </ExpansionPanelActions>
+                    </ExpansionPanel>
+                </div>
+
+
+                <div>
+                    <Button variant="contained" size="small" color="secondary">Deactivate</Button>
+                </div>
+
+                {/* <form novalidate>
                     <div id='container'>
                         <label>Profile Image:</label>
                         <div id='first'>
@@ -104,7 +229,7 @@ export class SettingsComponent extends React.Component {
                             <button class="" type="button" onClick={this.updateProfile}>Update Profile</button>
                         </div>
                     </div>
-                </form>
+                </form> */}
             </div>
         );
     }
